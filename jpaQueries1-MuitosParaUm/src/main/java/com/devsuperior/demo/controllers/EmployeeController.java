4a904fab458f6f062b2673883604e5021d1e043f
Ital023 <contadoitalo911@gmail.com>
2024-10-21 21:54:10 -0300

    feat: Pageable and All

--- jpaQueries1-MuitosParaUm/src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
+++ jpaQueries1-MuitosParaUm/src/main/java/com/devsuperior/demo/controllers/EmployeeController.java
@@ -3,6 +3,8 @@ package com.devsuperior.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,12 @@ public class EmployeeController {
 	
 	@GetMapping
 	public List<Employee> findAll() {
-		return employeeRepository.findAll();
+		return employeeRepository.searchAll();
 	}
+
+	@GetMapping(value = "/pageable")
+	public Page<Employee> findAllPageable(Pageable pageable) {
+		return employeeRepository.searchAll(pageable);
+	}
+
 }
