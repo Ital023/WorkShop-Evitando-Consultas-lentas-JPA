5a829764b836e306197be5b11994ffab0897c5ad
Ital023 <contadoitalo911@gmail.com>
2024-10-21 21:53:49 -0300

    feat: JOIN FETCH Many To One

--- jpaQueries1-MuitosParaUm/src/main/java/com/devsuperior/demo/repositories/EmployeeRepository.java
+++ jpaQueries1-MuitosParaUm/src/main/java/com/devsuperior/demo/repositories/EmployeeRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.demo.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.demo.entities.Employee;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "SELECT obj FROM Employee obj JOIN FETCH obj.department")
+    List<Employee> searchAll();
+
+    @Query(value = "SELECT obj FROM Employee obj JOIN FETCH obj.department",
+    countQuery = "SELECT COUNT(obj) FROM Employee obj JOIN obj.department")
+    Page<Employee> searchAll(Pageable pageable);
 
 }
